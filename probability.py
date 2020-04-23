23c309b31037c221c0cb65cc78534c11e606128a
vivia618 <vivia618@hotmail.com>
2020-04-23 22:28:03 +0200

    Add solutions to the Coin Flips and Die Rolls

--- probability.py
+++ probability.py
@@ -1,22 +1,10 @@
-#!/usr/bin/env python
-# coding: utf-8
-
 # # Coin Flips and Die Rolls
 # Use NumPy to create simulations and compute proportions for the following outcomes. The first one is done for you.
-# 
 # **Please note again that we are using 0 to represent heads, and 1 to represent tails.**
-
-# In[ ]:
-
-
 # import numpy
-
+import numpy as np
 
 # ### 1. Two fair coin flips produce exactly two heads
-
-# In[ ]:
-
-
 # simulate 1 million tests of two fair coin flips
 tests = np.random.randint(2, size=(int(1e6), 2))
 
@@ -24,59 +12,43 @@ tests = np.random.randint(2, size=(int(1e6), 2))
 test_sums = tests.sum(axis=1)
 
 # proportion of tests that produced exactly two heads
-(test_sums == 0).mean()
+print((test_sums == 0).mean())
 
 
 # ### 2. Three fair coin flips produce exactly one head
-
-# In[ ]:
-
-
 # simulate 1 million tests of three fair coin flips
-tests = 
+tests = np.random.randint(2, size = (int(1e6), 3))
 
 # sums of all tests
-test_sums = 
+test_sums = tests.sum(axis = 1)
 
 # proportion of tests that produced exactly one head
-
+print((test_sums == 1).mean())
 
 # ### 3. Three biased coin flips with P(H) = 0.6 produce exactly one head
-
-# In[ ]:
-
-
 # simulate 1 million tests of three biased coin flips
 # hint: use np.random.choice()
-tests = 
+tests = np.random.choice(2,size =(int(1e6), 3), p = [0.6,0.4])
 
 # sums of all tests
-test_sums = 
+test_sums = tests.sum(axis = 1)
 
 # proportion of tests that produced exactly one head
-
+print((test_sums == 1).mean())
 
 # ### 4. A die rolls an even number
-
-# In[ ]:
-
-
 # simulate 1 million tests of one die roll
-tests = 
+tests = np.random.randint(1,7, size = int(1e6))
 
 # proportion of tests that produced an even number
-
+print((tests % 2 == 0).mean())
 
 # ### 5. Two dice roll a double
-
-# In[ ]:
-
-
 # simulate the first million die rolls
-first = 
+first = np.random.randint(1,7, size = int(1e6))
 
 # simulate the second million die rolls
-second = 
+second = np.random.randint(1,7, size = int(1e6))
 
 # proportion of tests where the 1st and 2nd die rolled the same number
-
+print((first != second).mean())
